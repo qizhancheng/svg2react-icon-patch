6a2ccf1881ef4c5327bd99b64c94eaa37c875fbb
qizhancheng <1614101071@qq.com>
2019-08-08 15:05:33 +0800

    添加i标签

--- src/build-icons.js
+++ src/build-icons.js
@@ -1,62 +1,85 @@
-const fs = require('fs-extra');
-const path = require('path');
-const glob = require('glob');
-const svgToComponent = require('./lib/svg2component');
-const optimizeSvg = require('./lib/svg-optimizer');
+const fs = require("fs-extra");
+const path = require("path");
+const glob = require("glob");
+const svgToComponent = require("./lib/svg2component");
+const optimizeSvg = require("./lib/svg-optimizer");
 
-const componentsDirName = 'components';
+const componentsDirName = "components";
 
-module.exports = ({inputDir, outputDir, typescript, monochrome, namedExport}) => {
-  if (!inputDir || !outputDir) {
-    throw new Error('Input and output dirs not specified');
-  }
-  const icons = glob.sync(`${inputDir}/**/*.svg`);
-  return processIcons(icons, outputDir, {isTypeScriptOutput: typescript, monochrome, namedExport});
+module.exports = ({
+    inputDir,
+    outputDir,
+    typescript,
+    monochrome,
+    namedExport
+}) => {
+    if (!inputDir || !outputDir) {
+        throw new Error("Input and output dirs not specified");
+    }
+    const icons = glob.sync(`${inputDir}/**/*.svg`);
+    return processIcons(icons, outputDir, {
+        isTypeScriptOutput: typescript,
+        monochrome,
+        namedExport
+    });
 };
 
 async function processIcons(filenames, outputDir, options) {
-  fs.removeSync(outputDir);
-  fs.mkdirsSync(path.join(outputDir, componentsDirName));
+    fs.removeSync(outputDir);
+    fs.mkdirsSync(path.join(outputDir, componentsDirName));
 
-  const componentNames = await Promise.all(
-    filenames.map(icon => processIcon(icon, outputDir, options))
-  );
+    const componentNames = await Promise.all(
+        filenames.map(icon => processIcon(icon, outputDir, options))
+    );
 
-  createIndexFile(componentNames, outputDir, options);
+    createIndexFile(componentNames, outputDir, options);
 }
 
 async function processIcon(svgPath, outputDir, options) {
-  const name = path.basename(svgPath, '.svg');
-  const filename = name + (options.isTypeScriptOutput ? '.tsx' : '.js');
-  const relativePath = path.join(componentsDirName, filename);
-  const absolutePath = path.join(outputDir, relativePath);
+    const name = path.basename(svgPath, ".svg");
+    const filename = name + (options.isTypeScriptOutput ? ".tsx" : ".js");
+    const relativePath = path.join(componentsDirName, filename);
+    const absolutePath = path.join(outputDir, relativePath);
 
-  try {
-    const svg = fs.readFileSync(svgPath, 'utf-8');
-    const optimizedSvg = await optimizeSvg(svg, options.monochrome);
-    const componentCode = svgToComponent(name, optimizedSvg, options);
-    fs.writeFileSync(absolutePath, componentCode, 'utf-8');
-    console.log(`Created: ${relativePath}`);
-  } catch (err) {
-    console.error(`Failed to generate component ${name}. Error: ${err}`);
-  }
+    try {
+        const svg = fs.readFileSync(svgPath, "utf-8");
+        const optimizedSvg = await optimizeSvg(svg, options.monochrome);
+        const componentCode = svgToComponent(name, optimizedSvg, options);
+        fs.writeFileSync(absolutePath, componentCode, "utf-8");
+        console.log(`Created: ${relativePath}`);
+    } catch (err) {
+        console.error(`Failed to generate component ${name}. Error: ${err}`);
+    }
 
-  return name;
+    return name;
 }
 
 function createIndexFile(componentNames, outputDir, options) {
-  const code = componentNames.length ? [
-    '/* eslint-disable */',
-    '/* tslint:disable */',
-    componentNames.map(name =>
-      `export {${options.namedExport ? '' : 'default as '}${name}} from './${componentsDirName}/${name}';`
-    ).join('\n'),
-    '/* tslint:enable */',
-    '/* eslint-enable */',
-    ''
-  ].join('\n') : '\n';
+    const code = componentNames.length
+        ? [
+              "var ENV = process.env.NODE_ENV;",
+              "if (ENV !== 'production' && ENV !== 'test' && typeof console !== 'undefined' && console.warn && typeof window !== 'undefined') {",
+              "// tslint:disable-next-line:no-console",
+              "console.warn('You are using a whole package of yui-icon, ' + 'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.')",
+              "}",
+              "\n\n",
+              "/* eslint-disable */",
+              "/* tslint:disable */",
+              componentNames
+                  .map(
+                      name =>
+                          `export {${
+                              options.namedExport ? "" : "default as "
+                          }${name}} from './${componentsDirName}/${name}';`
+                  )
+                  .join("\n"),
+              "/* tslint:enable */",
+              "/* eslint-enable */",
+              ""
+          ].join("\n")
+        : "\n";
 
-  const filename = 'index' + (options.isTypeScriptOutput ? '.ts' : '.js');
-  fs.writeFileSync(path.join(outputDir, filename), code, 'utf-8');
-  console.log(`Created: ${filename}`);
+    const filename = "index" + (options.isTypeScriptOutput ? ".ts" : ".js");
+    fs.writeFileSync(path.join(outputDir, filename), code, "utf-8");
+    console.log(`Created: ${filename}`);
 }
--- src/lib/svg2component.js
+++ src/lib/svg2component.js
@@ -1,94 +1,95 @@
-const cheerio = require('cheerio');
-const forEach = require('lodash.foreach');
-const camelCase = require('lodash.camelcase');
-const esformatter = require('esformatter');
-esformatter.register(require('esformatter-jsx'));
+const cheerio = require("cheerio");
+const forEach = require("lodash.foreach");
+const camelCase = require("lodash.camelcase");
+const esformatter = require("esformatter");
+esformatter.register(require("esformatter-jsx"));
 
 module.exports = (name, svg, options) => {
-  const $ = cheerio.load(svg, {
-    xmlMode: true
-  });
-  const $svg = $('svg');
-  toReactAttributes($svg, $);
-  const children = $svg.html();
-  const viewBox = $svg.attr('viewBox') ? `viewBox="${$svg.attr('viewBox')}"` : '';
-  const widthFromSvg = $svg.attr('width') || '1em';
-  const heightFromSvg = $svg.attr('height') || '1em';
-  const code = options.isTypeScriptOutput ?
-    `
+    const $ = cheerio.load(svg, {
+        xmlMode: true
+    });
+    const $svg = $("svg");
+    toReactAttributes($svg, $);
+    const children = $svg.html();
+    const viewBox = $svg.attr("viewBox")
+        ? `viewBox="${$svg.attr("viewBox")}"`
+        : "";
+    const code = options.isTypeScriptOutput
+        ? `
       import * as React from 'react';
       export interface ${name}Props extends React.SVGAttributes<SVGElement> {
-        size?: string;
       }
-      ${options.namedExport ? 'export ' : ''}const ${name}: React.SFC<${name}Props> = ({size, ...props}) => (
-        <svg
-          ${viewBox}
-          fill="currentColor"
-          width={size || "${widthFromSvg}"}
-          height={size || "${heightFromSvg}"}
-          {...props}
-        >
-          ${children}
-        </svg>
+      ${
+          options.namedExport ? "export " : ""
+      }const ${name}: React.SFC<${name}Props> = ({className, ...props}) => (
+				<i className={\`anticon\$\{className ? " " + className : ""}\`}>
+					<svg
+						${viewBox}
+						fill="currentColor"
+						width="1em"
+						height="1em"
+						{...props}
+					>
+						${children}
+					</svg>
+				</i>
       );
-      ${name}.displayName = '${name}';
-      ${options.namedExport ? '' : `export default ${name};`}
-    ` :
+      ${options.namedExport ? "" : `export default ${name};`}
     `
-      import PropTypes from 'prop-types';
+        : `
       import React from 'react';
-      ${options.namedExport ? 'export ' : ''}const ${name} = ({size, ...props}) => (
+      ${
+          options.namedExport ? "export " : ""
+      }const ${name} = ({className, ...props}) => (
+				<i className={\`anticon\$\{className ? " " + className : ""}\`}>
         <svg
           ${viewBox}
           fill="currentColor"
-          width={size || "${widthFromSvg}"}
-          height={size || "${heightFromSvg}"}
+          width="1em"
+          height="1em"
           {...props}
         >
           ${children}
-        </svg>
+				</svg>
+				</i>
       );
-      ${name}.displayName = '${name}';
-      ${name}.propTypes = {
-        size: PropTypes.string
-      }
-      ${options.namedExport ? '' : `export default ${name};`}
+      ${options.namedExport ? "" : `export default ${name};`}
     `;
 
-  return [
-    '/* eslint-disable */',
-    '/* tslint:disable */',
-    esformatter.format(code).trim(),
-    '/* tslint:enable */',
-    '/* eslint-enable */',
-    ''
-  ].join('\n');
+    return [
+        "/* eslint-disable */",
+        "/* tslint:disable */",
+        esformatter.format(code).trim(),
+        "/* tslint:enable */",
+        "/* eslint-enable */",
+        ""
+    ].join("\n");
 };
 
-const resetIfNotNone = val => val === 'none' ? 'none' : 'currentColor';
-const attributesToRename = {'xlink:href': 'xlinkHref', class: 'className'};
-const attributesToReplace = {fill: resetIfNotNone, stroke: resetIfNotNone};
+const resetIfNotNone = val => (val === "none" ? "none" : "currentColor");
+const attributesToRename = { "xlink:href": "xlinkHref", class: "className" };
+const attributesToReplace = { fill: resetIfNotNone, stroke: resetIfNotNone };
 
 function toReactAttributes($el, $) {
-  forEach($el.attr(), (val, name) => {
-    if (attributesToReplace[name]) {
-      $el.attr(name, attributesToReplace[name](val));
+    forEach($el.attr(), (val, name) => {
+        if (attributesToReplace[name]) {
+            $el.attr(name, attributesToReplace[name](val));
+        }
+
+        if (name.indexOf("-") === -1 && !attributesToRename[name]) {
+            return;
+        }
+
+        const newName = attributesToRename[name] || camelCase(name);
+        $el.attr(newName, val).removeAttr(name);
+    });
+
+    if ($el.children().length === 0) {
+        return false;
     }
 
-    if (name.indexOf('-') === -1 && !attributesToRename[name]) {
-      return;
-    }
-
-    const newName = attributesToRename[name] || camelCase(name);
-    $el.attr(newName, val).removeAttr(name);
-  });
-
-  if ($el.children().length === 0) {
-    return false;
-  }
-
-  $el.children().each((index, el) => {
-    const $child = $(el);
-    toReactAttributes($child, $);
-  });
+    $el.children().each((index, el) => {
+        const $child = $(el);
+        toReactAttributes($child, $);
+    });
 }
